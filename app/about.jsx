b8ca4662cb07defc8272ec8f2263c72b58009a09
Musa Muhammad <musaawwaletudaye@gmail.com>
2024-05-28 23:51:38 +0100

    feat: spinned up expo project & installed expo router

--- app/about.jsx
+++ app/about.jsx
@@ -8,7 +8,8 @@ const about = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 30 }}>about</Text>
-      <Button onPress={() => router.back()} title="go back"></Button>
+      <Button onPress={() => router.back()} title="go back home"></Button>
+      <Button onPress={() => router.back()}></Button>
     </View>
   );
 };
