e8f052aa9f4489583f6f62a1d264a6e9c4d1fda6
Etudaye <musaawwaletudaye@gmail.com>
2024-07-04 23:46:51 +0100

    Update about.jsx
    perfect

--- app/about.jsx
+++ app/about.jsx
@@ -7,8 +7,8 @@ const router = useRouter();
 const about = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text style={{ fontSize: 35 }}>About Page.xyz</Text>
-      <Button onPress={() => router.back()} title="go back Home"></Button>
+      <Text style={{ fontSize: 35 }}>About Page</Text>
+      <Button onPress={() => router.back()} title="Go back Home"></Button>
       <Button onPress={() => router.back()}></Button>
     </View>
   );
