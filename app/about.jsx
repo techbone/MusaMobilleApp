5a9094d2ad48ccf8c2b36307170e8776b2cdc52c
Etudaye <musaawwaletudaye@gmail.com>
2024-07-05 23:42:18 +0100

    Update about.jsx

--- app/about.jsx
+++ app/about.jsx
@@ -8,7 +8,7 @@ const about = () => {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text style={{ fontSize: 35 }}>About Page</Text>
-      <Button onPress={() => router.back()} title="Go back Home"></Button>
+      <Button onPress={() => router.back()} title="Go Back Home"></Button>
       <Button onPress={() => router.back()}></Button>
     </View>
   );
